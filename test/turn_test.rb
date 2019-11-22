00577e241440699d61b468b7940fdb9d080b08d5
Jordan Sewell <jrsewell400@gmail.com>
2019-11-22 12:38:08 -0700

    Finish turn and turn_test files

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -9,9 +9,16 @@ class TurnTest < Minitest::Test
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
 
+    assert_instance_of Card, card
     assert_instance_of Turn, turn
   end
 
+  def test_if_incorrect
+    card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
+    turn = Turn.new("Saturn", card)
 
+    assert_equal "Mercury", card.answer
+    assert_equal "Saturn", turn.guess
+  end
 
 end
