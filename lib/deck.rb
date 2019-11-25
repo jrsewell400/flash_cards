1a6fa601505a0f34ca3484337b927e4f45d8664a
Jordan Sewell <jrsewell400@gmail.com>
2019-11-25 01:49:12 -0500

    Add additional work to deck.rb and round.rb files

--- lib/deck.rb
+++ lib/deck.rb
@@ -6,7 +6,7 @@ class Deck
     @card_1 = card_1
     @card_2 = card_2
     @card_3 = card_3
-    @cards = [card_1, card_2, card_3]
+    @cards = cards
 
   end
 
@@ -14,15 +14,14 @@ class Deck
     @card_1 = "What is the capital of Alaska?", "Juneau", :Geography
     @card_2 = "The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM
     @card_3 = "Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM
+    cards = [@card_1, @card_2, @card_3]
 
   end
 
   def count
-    cards.count
   end
 
   def cards_in_category
-
   end
 
 end
